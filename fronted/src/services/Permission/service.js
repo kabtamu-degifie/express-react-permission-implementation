9ce505305076e69d80dcf5d6b94795bea73dde0f
kabtamu Degifie <kabtamu.degifie@wku.edu.et>
2022-07-26 06:09:26 +0300

    added reducers

---
+++ fronted/src/services/Permission/service.js
@@ -0,0 +1,14 @@
+import http from "../../libs/axios";
+import { getToken } from "../../libs/local-storage";
+
+const API_URL = "/v1/permissions";
+
+const fetch = async () => {
+  http.setHeader(getToken());
+  const response = await http.get(API_URL);
+  return response.data;
+};
+
+const service = { fetch };
+
+export default service;
---
+++ fronted/src/services/Role/service.js
@@ -0,0 +1,32 @@
+import http from "../../libs/axios";
+import { getToken } from "../../libs/local-storage";
+
+const API_URL = "/v1/roles";
+
+const fetch = async () => {
+  http.setHeader(getToken());
+  const response = await http.get(API_URL);
+  return response.data;
+};
+
+const create = async (role) => {
+  http.setHeader(getToken());
+  const response = await http.post(API_URL, role);
+  return response.data;
+};
+
+const update = async (params) => {
+  http.setHeader(getToken());
+  const response = await http.put(`${API_URL}/${params.id}`, params.role);
+  return response.data;
+};
+
+const remove = async (params) => {
+  http.setHeader(getToken());
+  const response = await http.delete(`${API_URL}/${params.id}`, params.role);
+  return response.data;
+};
+
+const service = { fetch, create, update, remove };
+
+export default service;
