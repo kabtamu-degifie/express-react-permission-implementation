5e4c6ad3c6be2e6ee556c4797927a8a3078b5ba0
kabtamu Degifie <kabtamu.degifie@wku.edu.et>
2022-07-28 12:51:55 +0300

    update drawer

--- frontend/src/layouts/PageContainer.js
+++ frontend/src/layouts/PageContainer.js
@@ -1,8 +1,18 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { Box } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
 import MiniAppBar from "./MiniAppBar";
 import MiniDrawer from "./MiniDrawer";
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  ...theme.mixins.toolbar,
+}));
 
 function PageContainer() {
   const [open, setOpen] = React.useState(false);
@@ -20,6 +30,7 @@ function PageContainer() {
       <MiniAppBar handleDrawerOpen={handleDrawerOpen} open={open} />
       <MiniDrawer handleDrawerClose={handleDrawerClose} open={open} />
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <DrawerHeader />
         <Outlet />
       </Box>
     </Box>
